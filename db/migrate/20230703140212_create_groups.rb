a7cd389162127a9b043e27092ff2160f8a6e26c5
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-07-03 14:10:10 +0000

    Run migration

--- db/migrate/20230703140212_create_groups.rb
+++ db/migrate/20230703140212_create_groups.rb
@@ -2,7 +2,7 @@ class CreateGroups < ActiveRecord::Migration[7.0]
   def change
     create_table :groups do |t|
       t.string :name
-      t.references :icon, null: false, foreign_key: true
+      t.references :icon, null: false, foreign_key: { to_table: :users }
 
       t.timestamps
     end
--- db/migrate/20230703140319_create_pockets.rb
+++ db/migrate/20230703140319_create_pockets.rb
@@ -1,7 +1,7 @@
 class CreatePockets < ActiveRecord::Migration[7.0]
   def change
     create_table :pockets do |t|
-      t.references :author, null: false, foreign_key: true
+      t.references :author, null: false, foreign_key: { to_table: :users }
       t.string :name
 
       t.timestamps
