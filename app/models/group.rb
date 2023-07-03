7b0eca1b255cb328a5eaf113b3d830fe613c347c
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-07-03 17:07:47 +0000

    Update associations and path

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,4 @@
 class Group < ApplicationRecord
-  belongs_to :icon, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :icon, class_name: 'User', foreign_key: 'icon_id'
   has_many :pockets
 end
